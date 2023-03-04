70ee752ea1863c99a0f5d9a4097a43d3268c2cad
Dan <107619540+MrEzerYT@users.noreply.github.com>
2023-03-04 11:38:35 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+class Auth:
+    discord_auth = {
+        "debug" : "debug-token",
+        "release" : "token-release"
+    }
+
+class Color:
+    primary = 0x3EE2B7
+    transparent = 0x2F3136
+    blurple_old = 0x7289DA
+    blurple = 0x586F2
