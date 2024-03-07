b90c0385cdbbb3306182529e6a09c2aea4f2af17
Dan <107619540+MrEzerYT@users.noreply.github.com>
2024-03-07 09:46:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
     try:
         client.run(token)
     except Exception as e:
-        client.logger.warning('При запуске произшла непредвиденная ошибка!')
+        client.logger.warning('При запуске произшла непредвиденная ошибка!') # скоро добавлю типы ошибок, т.к. это будет удобнее для вас
         client.logger.exception(f'Ошибка: {e}')
