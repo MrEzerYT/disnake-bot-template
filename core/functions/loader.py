077c37805c1f06d4bd74af9d0592bf677236be20
Dan <107619540+MrEzerYT@users.noreply.github.com>
2024-03-07 09:27:24 +0300

    доделал коммит🥱

--- core/functions/loader.py
+++ core/functions/loader.py
@@ -1,5 +1,10 @@
 from os import listdir
 
 def ModulesLoader(self):
-    for filename on listdir('./modules'):
-        #остальгле потом 
+    for filename in listdir("core/modules"):
+        if filename.endswith(".py") and not filename.startswith("_"):
+            try:
+                self.load_extension(f"core.modules.{filename[:-3]}"); self.logger.success(f"Module {filename} is loaded!")
+            except Exception as e:
+                self.logger.error(f"Module {filename} fucked up: {e}")
+    self.logger.success("All modules loaded!")
