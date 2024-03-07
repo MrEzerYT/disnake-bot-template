bdc3736f825cbb72669871444fa52087abf4397a
Dan <107619540+MrEzerYT@users.noreply.github.com>
2024-03-07 09:38:34 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,1 +1,11 @@
-ща будет конфиг
+prefix = "."
+
+class Auth:
+    discord_auth = {
+        "debug" : "Токен с сайта discord.com/developers тут указывается тестовый бот",
+        "release" : "Бот для релиза, который полностью готов"
+    }
+
+
+Status = ['.help | flyrix.space',
+          'разработчик: @mrezer'] # можете добавить любой статус, он будет ставиться на рандом в main.py
