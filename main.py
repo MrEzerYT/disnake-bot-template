5b5ec41e945b751abf07c3aea6858c838a6910e1
Dan <107619540+MrEzerYT@users.noreply.github.com>
2024-03-07 09:40:43 +0300

    новый боти))))))))))))/)///////))))))))))))🥱

--- main.py
+++ main.py
@@ -1,87 +1,17 @@
-import time
+from core.config import Status; from core.libs import *
 
-import datetime
-
-def time4logs():
-
-    return f'[{datetime.datetime.now().strftime("%d.%m.%Y %h:%M:%S")}]'
-
-print(time4logs(), "Бот запускается")
-
-start = time.time()
-
-import disnake #disnake весь код будет на нём
-
-from disnake.ext import commands #команды
-
-import asyncio
-
-from asyncio import sleep
-
-import os #файлы
-
-import config
-
-import oauth
-
-from config import Auth
-
-print(time4logs(), "Библиотеки импортированы")
-
-#Начало кода бота
-
-release = oauth.release
-
-if release:
-
-    token = Auth.discord_auth["release"]
-
-else:
-
-    token = Auth.discord_auth["debug"]
-
-intents = disnake.Intents().all()
-
-bot = commands.Bot(intents = intents, command_prefix = "?")
-
-@bot.event
-
+@client.event
 async def on_ready():
+    await status.start()
 
-    print(f"{time4logs()}Бот запущен под ником {bot.user}")
+@loop(seconds=60.0)
+async def status():
+    current_status = choice(Status)
+    await client.change_presence(activity=Game(name=current_status, platform="Linux"))
 
-    #Статус
-
-    while True:
-
-        await bot.change_presence(activity=disnake.Game(name="в пинг понг"))
-
-        await sleep(15)
-
-        await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching, name="за серверами"))
-
-        await sleep(15)
-
-for filename in os.listdir("./cogs"):
-
-    if filename.endswith(".py") and not filename.startswith("_"):
-
-        bot.load_extension(f'cogs.{filename[:-3]}')
-
-        print(f"[INFO] Module {filename} is loaded!")
-
-@bot.slash_command()
-
-async def reload(ctx, extention):
-
-    if ctx.author.id in [578533097293873162]:
-
-        bot.unload_extension(f"cogs.{extention}")
-
-        bot.load_extension(f"cogs.{extention}")
-
-        embed = disnake.Embed(color = disnake.Color.green(), title = "Reload", description = f"Module ` {extention} ` successfully reloaded")
-
-        await ctx.send(embed=embed)
-
-bot.run(token)
+if __name__ == "__main__":
+    try:
+        client.run(token)
+    except Exception as e:
+        client.logger.warning('При запуске произшла непредвиденная ошибка!')
+        client.logger.exception(f'Ошибка: {e}')
