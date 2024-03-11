bca172b1a758b3211bd31b14148a5e71fde33fb1
Dan <107619540+MrEzerYT@users.noreply.github.com>
2024-03-11 16:05:04 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 sudo apt update && apt upgrade
 apt install python3
 apt install python3-pip
+apt install screen
 echo "Python3 успешно установлен, установка модулей бота"
 python3 -m pip install -r requirements.txt
 clear
