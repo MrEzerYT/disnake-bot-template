5589351160ecc5c1648ff16995688f7182062d3d
Dan <107619540+MrEzerYT@users.noreply.github.com>
2024-05-14 15:36:20 +0300

    Update loader.py

--- core/functions/loader.py
+++ core/functions/loader.py
@@ -1,12 +1,17 @@
-from os import listdir, system; from pathlib import Path
+from os import listdir; from pathlib import Path
 
-def ModulesLoader(client):
+def ModulesLoader(client, path):
     """
     Подгружает все cogs из директории core/modules.
 
     Для использования вам необходимо настроить logger от loguru и создать отдельный класс бота.
     """
-    for module in listdir("core/modules"):
+    if Path(path).exists():
+        client.logger.debug(f"Directory {path} exists!")
+    else:
+        Path(path).mkdir(parents=True, exist_ok=False)
+        client.logger.debug(f"Directory {path} was created!")
+    for module in listdir(path):
         if module.endswith(".py") and not module.startswith("_"):
             try:
                 client.load_extension(f"core.modules.{module[:-3]}"); client.logger.success(f"» Module {module} is loaded!")
